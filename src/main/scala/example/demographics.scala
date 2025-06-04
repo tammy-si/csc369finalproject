9e6e850a632ba2a2e6172f549c682862fdf477cd
tammy-si <sitammy6@gmail.com>
2025-06-03 20:43:34 -0700

    demographics code

---
+++ src/main/scala/example/demographics.scala
@@ -0,0 +1,75 @@
+package example
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext._
+
+import scala.io._
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+import scala.collection._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{avg, col, round, desc, first}
+
+object demographics {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("FinalProject")
+      .master("local[*]")
+      .getOrCreate()
+    val sc = spark.sparkContext
+    val inputPath = args(0)
+    val outputPath = args(1)
+    val rawdiabetesDF = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(inputPath)
+
+    val healthRates = rawdiabetesDF
+      .withColumn("DiabetesClass", col("Diabetes").cast("int"))
+      .groupBy("DiabetesClass")
+      .agg(
+        round(avg(col("Smoker")) * 100, 1).alias("Smoker_Percent"),
+        round(avg(col("Fruits")) * 100, 1).alias("Fruits_Percent"),
+        round(avg(col("PhysActivity")) * 100, 1).alias("PhysActivity_Percent"),
+        round(avg(col("HighChol")) * 100, 1).alias("HighChol_Percent"),
+        round(avg(col("HighBP")) * 100, 1).alias("HighBP_Percent")
+      )
+      .orderBy("DiabetesClass")
+    healthRates.show()
+    // mode code import org.apache.spark.sql.functions._
+
+    val ageModes = rawdiabetesDF
+      .withColumn("DiabetesClass", col("Diabetes").cast("int"))
+      .groupBy("DiabetesClass", "Age")
+      .count()
+      .orderBy(col("DiabetesClass"), desc("count"))
+      .groupBy("DiabetesClass")
+      .agg(first("Age").alias("Most_Common_Age_Group"))
+
+    ageModes.show()
+
+    val incomeModes = rawdiabetesDF
+      .withColumn("DiabetesClass", col("Diabetes").cast("int"))
+      .groupBy("DiabetesClass", "Income")
+      .count()
+      .orderBy(col("DiabetesClass"), desc("count"))
+      .groupBy("DiabetesClass")
+      .agg(first("Income").alias("Most_Common_Income_Group"))
+
+    incomeModes.show()
+
+    val educationModes = rawdiabetesDF
+      .withColumn("DiabetesClass", col("Diabetes").cast("int"))
+      .groupBy("DiabetesClass", "Education")
+      .count()
+      .orderBy(col("DiabetesClass"), desc("count"))
+      .groupBy("DiabetesClass")
+      .agg(first("Education").alias("Most_Common_Education_Level"))
+
+    educationModes.show()
+  }
+}
